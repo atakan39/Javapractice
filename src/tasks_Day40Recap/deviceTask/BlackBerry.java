e136da346b4e26dea0d73293a22fa58a58902c3d
atakantaskin <atakantaskin91@gmail.com>
2022-01-15 16:41:04 -0500

    BlackBerry class is done

---
+++ src/tasks_Day40Recap/deviceTask/BlackBerry.java
@@ -0,0 +1,10 @@
+package tasks_Day40Recap.deviceTask;
+
+public class BlackBerry extends Phone{
+
+    public BlackBerry(String model, double price) {
+        super("BlackBerry", model, price);
+    }
+
+
+}
