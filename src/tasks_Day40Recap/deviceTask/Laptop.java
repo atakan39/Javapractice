ae72ff67059f191d122b91bd81ee879eb163ff00
atakantaskin <atakantaskin91@gmail.com>
2022-01-15 16:49:07 -0500

    Laptop class is done

---
+++ src/tasks_Day40Recap/deviceTask/Laptop.java
@@ -0,0 +1,8 @@
+package tasks_Day40Recap.deviceTask;
+
+public class Laptop extends Computer{
+
+    public Laptop(String brand, String model, double price, boolean hasBattery, double GPU, boolean hasHdmlPort) {
+        super(brand, model, price, hasBattery, GPU, hasHdmlPort);
+    }
+}
