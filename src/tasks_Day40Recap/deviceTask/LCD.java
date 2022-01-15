e6bd78aeb70b8d60a36337fb296729a8e4976bb7
atakantaskin <atakantaskin91@gmail.com>
2022-01-15 16:50:17 -0500

    Lcd class is done

---
+++ src/tasks_Day40Recap/deviceTask/LCD.java
@@ -0,0 +1,8 @@
+package tasks_Day40Recap.deviceTask;
+
+public class LCD extends TV{
+    public LCD(String brand, String model, double price, boolean hasBattery, double monitorSize, boolean isSmartTv) {
+        super(brand, model, price, hasBattery, monitorSize, isSmartTv);
+    }
+
+}
