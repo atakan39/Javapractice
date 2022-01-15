33ca7ec43f54f6e6eeae3d514b412be809ed6d89
atakantaskin <atakantaskin91@gmail.com>
2022-01-15 16:44:42 -0500

    PC class is done

---
+++ src/tasks_Day40Recap/deviceTask/PersonalComputer.java
@@ -0,0 +1,10 @@
+package tasks_Day40Recap.deviceTask;
+
+public class PersonalComputer extends Computer{
+
+    public PersonalComputer(String brand, String model, double price, boolean hasBattery, double GPU, boolean hasHdmlPort) {
+        super(brand, model, price, hasBattery, GPU, hasHdmlPort);
+    }
+
+
+}
