4f9d254951bb3bf12e85c135159a6e625e57da0d
atakantaskin <atakantaskin91@gmail.com>
2022-01-15 16:14:38 -0500

    phone class done

---
+++ src/tasks_Day40Recap/deviceTask/Phone.java
@@ -0,0 +1,17 @@
+package tasks_Day40Recap.deviceTask;
+
+public class Phone extends Device{
+    public Phone(String brand, String model, double price) {
+        super(brand, model, price, true);
+    }
+
+    public void call(long phoneNum){
+        System.out.println(getBrand()+""+getModel()+" is calling ("+phoneNum+")");
+    }
+
+    public void text(long phoneNum){
+        System.out.println(getBrand()+""+getModel()+" is calling to ("+phoneNum+")");
+    }
+
+
+}
