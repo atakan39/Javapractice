2d52dde47a1e480af5b7b8b1a87f23e37da33738
atakantaskin <atakantaskin91@gmail.com>
2022-01-16 12:07:54 -0500

    Animal

--- src/tasks_Day38MethodOverriding/AnimalTask/Animal.java
+++ src/tasks_Day38MethodOverriding/AnimalTask/Animal.java
@@ -103,3 +103,29 @@ public class Animal {
         System.out.println(name+"is eating");
     }
 }
+/*
+@@@ Animal Task:
+        1. Create a class named Animal:
+                        Variables:
+                                name, breed, age, gender, size, color
+
+                        Add a constructor to set all the fields
+
+                        methods:
+                                eat()
+                                toString()
+
+        2. Create the followin sub classes of Animal and override the eat method:
+                        1. Cat
+                                        eat(): eats cat food
+
+                        2. Dog
+                                        eat(): eats dog food
+
+                        3. Tiger
+                                        eat(): eats deer
+
+                        4. Eagle
+                                        eat(): eats snake
+
+ */
