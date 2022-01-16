1ed0facf021f1546a9ed2db2367366c2b584d2f8
atakantaskin <atakantaskin91@gmail.com>
2022-01-16 09:55:36 -0500

    undergraduate student class is done

---
+++ src/tasks_Day40/studentTask/UndergraduateStudent.java
@@ -0,0 +1,14 @@
+package tasks_Day40.studentTask;
+
+public class UndergraduateStudent extends Student{
+
+    public UndergraduateStudent(String name, int age, char gender, int studentId, String fieldOfStudy, char grade, String schoolName) {
+        super(name, age, gender, studentId, fieldOfStudy, grade, schoolName);
+    }
+
+    @Override
+    public void study() {
+        System.out.println(getName()+"studying"+getFieldOfStudy());;
+    }
+
+}
