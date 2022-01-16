62403fa988df0cde52041852a0d35c4f6227adfe
atakantaskin <atakantaskin91@gmail.com>
2022-01-16 10:37:43 -0500

    parent class animal is done

---
+++ src/tasks_Day38MethodOverriding/AnimalTask/Animal.java
@@ -0,0 +1,105 @@
+package tasks_Day38MethodOverriding.AnimalTask;
+
+public class Animal {
+    private String name,breed,color,size;
+    private char gender;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.isEmpty()||name.isBlank()||name==null){
+            System.err.println("invalid name");
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        if(breed.isEmpty()||breed.isBlank()||breed==null){
+            System.err.println("invalid breed");
+            System.exit(1);
+        }
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if(color.isEmpty()||color.isBlank()||color==null){
+            System.err.println("invalid color");
+            System.exit(1);
+        }
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if(name.isEmpty()||name.isBlank()||name==null){
+            System.err.println("invalid name");
+            System.exit(1);
+        }
+        this.size = size;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        if(gender!='F' && gender!='M'){
+            System.err.println("invalid gender");
+            System.exit(1);
+        }
+        this.gender = gender;
+    }
+
+    public int getAge() {
+
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age<0){
+            System.err.println("invalid age");
+            System.exit(1);
+        }
+        this.age = age;
+    }
+
+    public Animal(String name, String breed, String color, String size, char gender, int age) {
+        setName(name);
+        setBreed(breed);
+        setColor(color);
+        setSize(size);
+        setGender(gender);
+        setAge(age);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", color='" + color + '\'' +
+                ", size='" + size + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                '}';
+    }
+
+    public void eat(){
+        System.out.println(name+"is eating");
+    }
+}
