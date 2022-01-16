c8e7af56971d671a45504906de82b637984cd412
atakantaskin <atakantaskin91@gmail.com>
2022-01-16 12:10:06 -0500

    developer class is done

---
+++ src/tasks_Day38MethodOverriding/EmployeeTask/Developer.java
@@ -0,0 +1,13 @@
+package tasks_Day38MethodOverriding.EmployeeTask;
+
+public class Developer extends Employee{
+
+    public Developer(String name, String jobTitle, String companyNAme, char gender, int age, int id, double salary) {
+        super(name, jobTitle, companyNAme, gender, age, id, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is fixing bugs");
+    }
+}
