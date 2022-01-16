612c875d62224a7adbdbd36414ae1aa85af10757
atakantaskin <atakantaskin91@gmail.com>
2022-01-16 12:07:45 -0500

    Employee

--- src/tasks_Day38MethodOverriding/EmployeeTask/Employee.java
+++ src/tasks_Day38MethodOverriding/EmployeeTask/Employee.java
@@ -63,6 +63,16 @@ public class Employee {
         this.salary = salary;
     }
 
+    public Employee(String name, String jobTitle, String companyNAme, char gender, int age, int id, double salary) {
+        setName(name);
+        setJobTitle(jobTitle);
+        setCompanyNAme(companyNAme);
+        setGender(gender);
+        setAge(age);
+        setId(id);
+        setSalary(salary);
+    }
+
     public void work(){
         System.out.println(name+" "+jobTitle+" is working");
     }
@@ -80,3 +90,29 @@ public class Employee {
                 '}';
     }
 }
+/*
+Employee Task:
+        1. Create a class named Employee
+                        Variables:
+                                name, age, gender, id, jobTitle, salaryName, comapnyName
+
+                        Add a constructor to set all the fields
+
+                        Methods:
+                                work()
+                                toString()
+
+
+        2. Create ethe following subclasses and override the work method in each subclass:
+
+                        1. Tester
+                        2. Developer
+                        3. ProductOwner
+                        4. BusinessAnalyst
+                        5. ScrumMaster
+                        6. Teacher
+                        7. Driver
+                        9. Pilot
+
+                if any adidtional field or methods needed, please add them
+ */
