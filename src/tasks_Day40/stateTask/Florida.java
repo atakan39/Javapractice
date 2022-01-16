977198cc0129db83823b7bc45dae081c91557470
atakantaskin <atakantaskin91@gmail.com>
2022-01-16 08:32:12 -0500

    florida class is done

---
+++ src/tasks_Day40/stateTask/Florida.java
@@ -0,0 +1,8 @@
+package tasks_Day40.stateTask;
+
+public class Florida extends States{
+
+    public Florida(String name, String abbreviation, String politicalParty, String governor, String senator, long population, double stateTax) {
+        super(name, abbreviation, politicalParty, governor, senator, population, stateTax);
+    }
+}
