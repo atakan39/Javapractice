fa032ed3689c87f420ae5bb7201558ce00be699b
atakantaskin <atakantaskin91@gmail.com>
2022-01-16 08:34:30 -0500

    Virginia class is done

---
+++ src/tasks_Day40/stateTask/Virginia.java
@@ -0,0 +1,7 @@
+package tasks_Day40.stateTask;
+
+public class Virginia extends States{
+    public Virginia(String name, String abbreviation, String politicalParty, String governor, String senator, long population, double stateTax) {
+        super(name, abbreviation, politicalParty, governor, senator, population, stateTax);
+    }
+}
